4fcdad3323213938a58f5eedc621792f4d0ce336
Louis Delia <delia.louis@gmail.com>
2023-04-10 19:11:06 -0400

    catch-all routes example with photos

---
+++ pages/users/[id]/[...albumid].js
@@ -0,0 +1,47 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function PostPage() {
+  const router = useRouter();
+  const { albumid } = router.query;
+
+  console.log(albumid);
+
+  const [photos, setPhotos] = useState(null);
+
+  useEffect(() => {
+    if (albumid && albumid.length) {
+      fetch("https://jsonplaceholder.typicode.com/albums/" + albumid[albumid.length - 1] +"/photos")
+        .then((response) => response.json())
+        .then((data) => {
+          setPhotos(data);
+        });
+    }
+  }, [albumid]);
+
+  return (
+    <>
+      <h1>This is the photos page!</h1>
+      {photos && (
+        <div>
+          <ul>
+            {photos.map((photo) => (
+              <li key={photo.id}>
+                <h2>{photo.title}</h2>
+                <img src={photo.thumbnailUrl} />
+                <a href={photo.url} target="_blank">
+                  See Photo
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {!albumid && (
+        <div>
+          <h2>There are currently no photos available.</h2>
+        </div>
+      )}
+    </>
+  );
+}
