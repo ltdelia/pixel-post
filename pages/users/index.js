a9a9cd313de5ce9a0ef374f5aefda4a3c125ea0f
Louis Delia <delia.louis@gmail.com>
2023-04-10 19:30:34 -0400

    dynamic routing for users landing page, with all users

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,13 +1,26 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function UsersLanding() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  });
+
   return (
     <>
       <h1>This is the landing page for users</h1>
       <ul>
-        <li>
-            <Link href="/users/1">User 1</Link>
-        </li>
+        {users && users.map((user) => (
+          <li key={user.id}>
+            <Link href={'/users/' + user.id}>{user.name}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
